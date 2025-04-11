ad681ac26de55d53f2b451a88d65c5e3309397a7
Gabriel Hall <adrastopoulos@gmail.com>
2025-04-11 15:47:09 -0400

    chore: remove outdated events api delegate impls

--- service/src/main/kotlin/com/terrier/service/event/EventsApiDelegateImpl.kt
+++ service/src/main/kotlin/com/terrier/service/event/EventsApiDelegateImpl.kt
@@ -17,36 +17,4 @@ class EventsApiDelegateImpl(
 ) : EventsApiDelegate {
     private val eventMapper = Mappers.getMapper(EventMapper::class.java)
 
-    override fun eventsEventIdDelete(eventId: UUID): ResponseEntity<Unit> {
-        eventRepository.deleteById(eventId)
-        return ResponseEntity(HttpStatus.NO_CONTENT)
-    }
-
-    override fun eventsEventIdGet(eventId: UUID): ResponseEntity<EventModel> {
-        val event = eventRepository.findByIdOrNull(eventId) ?: throw NotFoundException("Event with id $eventId not found")
-        return ResponseEntity.ok(eventMapper.toEventModel(event))
-    }
-
-    override fun eventsEventIdPut(eventId: UUID, eventModel: EventModel): ResponseEntity<EventModel> {
-        val existingEvent = eventRepository.findByIdOrNull(eventId)
-            ?: throw NotFoundException("Event with id $eventId not found")
-
-        val updatedEvent = eventMapper.toEvent(eventModel).apply {
-
-        }
-
-        val savedEvent = eventRepository.save(updatedEvent)
-        return ResponseEntity.ok(eventMapper.toEventModel(savedEvent))
-    }
-
-    override fun eventsGet(): ResponseEntity<List<EventModel>> {
-        val events = eventRepository.findAll().map { eventMapper.toEventModel(it) }
-        return ResponseEntity.ok(events)
-    }
-
-    override fun eventsPost(eventModel: EventModel): ResponseEntity<EventModel> {
-        val event = eventMapper.toEvent(eventModel)
-        eventRepository.save(event)
-        return ResponseEntity.status(HttpStatus.CREATED).body(eventMapper.toEventModel(event))
-    }
 }
